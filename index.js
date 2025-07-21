ea88e9601674226a00f13eead99cf8e14f99fade
reginaKandekere <reginakandekere25@gmail.com>
2025-07-20 19:38:59 -0700

    adds string literals for display of inputed text, makes use of local storage

--- index.js
+++ index.js
@@ -3,16 +3,38 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    renderLeads()
+})
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     renderLeads()
+    inputEl = ""
 })
 
 function renderLeads(){
     let listItems = ""
 for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>"
-}
-ulEl.innerHTML = listItems
+    listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
 }
 
