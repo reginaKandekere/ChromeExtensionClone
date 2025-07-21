8059656f1b7cc94b129fd1144f91cb8d70a1fa4b
reginaKandekere <reginakandekere25@gmail.com>
2025-07-20 19:57:12 -0700

    adds functionality for delete all button to clear the Dom when double clicked. adds styling for the delete all button

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
 
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     renderLeads()
 }
+
+deleteBtn.addEventListener("dblClick", function() {
+    localStorage.clear()
+    myLeads = []
+    renderLeads
+
+})
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
