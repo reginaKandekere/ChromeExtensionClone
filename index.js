32013f5a8709490065333fb36dffc1db105583f9
reginaKandekere <reginakandekere25@gmail.com>
2025-07-20 21:59:19 -0700

    adds manifest.json file, removes hardcoded code and makes it more dynamic for reusability. Adds a save rab btn to save a url in the local storage

--- index.js
+++ index.js
@@ -3,19 +3,27 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-
+const tabBtn = document.getElementById("tab-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    renderLeads(myLeads)
 }
+
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
 
 deleteBtn.addEventListener("dblClick", function() {
     localStorage.clear()
     myLeads = []
-    renderLeads
+    renderLeads(myLeads)
 
 })
 
@@ -23,22 +31,16 @@ inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    renderLeads()
+    renderLeads(myLeads)
 })
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    renderLeads()
-    inputEl = ""
-})
-
-function renderLeads(){
+function renderLeads(leads){
     let listItems = ""
-for (let i = 0; i < myLeads.length; i++) {
+for (let i = 0; i < leads.length; i++) {
     listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
